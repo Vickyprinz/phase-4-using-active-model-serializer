3ec70b177360a89267961f580c2ef95786e4cbee
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-16 14:57:37 +0300

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,9 +6,21 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+
+
   def show
     movie = Movie.find(params[:id])
     render json: movie
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
   end
 
   private
